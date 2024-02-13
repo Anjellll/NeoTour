a122a8774db79b88ebf8f80939830d1677840fd2
Anjella <anjellakattoobekova@gmail.com>
2024-02-14 01:47:30 +0600

    Established MVVM project structure

---
+++ NeoTour/Model/NetworkLayer/NetworkLayer.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkLayer.swift
+//  NeoTour
+//
+//  Created by anjella on 14/2/24.
+//
+
+import Foundation
---
+++ NeoTour/Modules/OnboardingWorkFlow/View/OnboardingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingViewController.swift
+//  NeoTour
+//
+//  Created by anjella on 13/2/24.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view...
+    }
+    
+    
+}
