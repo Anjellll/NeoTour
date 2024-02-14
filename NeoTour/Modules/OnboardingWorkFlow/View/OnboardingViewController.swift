8caaf1172a63a3666d14a764fbb668aba723b448
Anjella <anjellakattoobekova@gmail.com>
2024-02-15 00:51:45 +0600

    Designed the Onboardingpage

--- NeoTour/Modules/OnboardingWorkFlow/View/OnboardingViewController.swift
+++ NeoTour/Modules/OnboardingWorkFlow/View/OnboardingViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SnapKit
 
 class OnboardingViewController: UIViewController {
     
@@ -20,8 +21,121 @@ class OnboardingViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    private lazy var placeImage: UIImageView = {
+        var image = UIImageView()
+        image.contentMode = .scaleAspectFill
+        image.image = UIImage(named: "place_image")
+        image.layer.cornerRadius = 37
+        image.clipsToBounds = true
+        return image
+    }()
+    
+    private lazy var placeName: UILabel = {
+        let name = UILabel()
+        name.textColor = .black
+        name.text = """
+        Winter
+        Vacation Trips
+        """
+        name.numberOfLines = 0
+        name.lineBreakMode = .byWordWrapping
+        name.textAlignment = .left
+        name.font = .systemFont(ofSize: 36, weight: .bold)
+        return name
+    }()
+    
+    private lazy var placeDescription: UILabel = {
+        let name = UILabel()
+        name.textColor = .black
+        name.numberOfLines = 0
+        name.lineBreakMode = .byWordWrapping
+        name.textAlignment = .left
+        name.text = """
+        Enjoy your winter vacations with warmth
+        and amazing sightseeing on the mountains.
+        Enjoy the best experience with us!
+        """
+        name.font = .systemFont(ofSize: 16, weight: .regular)
+        return name
+    }()
+    
+    private lazy var letsGoButton: UIButton = {
+        let button = UIButton()
+        button.contentMode = .center
+        button.contentHorizontalAlignment = .left
+        button.isUserInteractionEnabled = true
+        button.setTitle("Let's Go", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 25
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
+        button.backgroundColor = UIColor(
+            red: CGFloat(0x6A) / 255.0,
+            green: CGFloat(0x62) / 255.0,
+            blue: CGFloat(0xB7) / 255.0,
+            alpha: 1.0
+        )
+        
+        let image = UIImage(systemName: "arrow.right")
+        button.setImage(image, for: .normal)
+        button.tintColor = .white
+        button.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        button.imageView?.contentMode = .scaleAspectFit
+        
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
+        view.backgroundColor = .white
+    }
+    
+    override func loadView() {
+        super.loadView()
+        setUpUI()
+    }
+}
+
+extension OnboardingViewController {
+    private func setUpUI() {
+        setUpSubviews()
+        setUpConstraints()
+    }
+    
+    private func setUpSubviews() {
+        view.addSubview(placeImage)
+        view.addSubview(placeName)
+        view.addSubview(placeDescription)
+        view.addSubview(letsGoButton)
+    }
+    
+    private func setUpConstraints() {
+        placeImage.snp.makeConstraints {
+            $0.top.equalTo(view.snp.top)
+            $0.leading.equalTo(view.snp.leading)
+            $0.trailing.equalTo(view.snp.trailing)
+            $0.width.equalTo(415)
+            $0.height.equalTo(480)
+        }
+        
+        placeName.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(512)
+            $0.leading.equalToSuperview().inset(16)
+            $0.width.equalTo(283)
+            $0.height.equalTo(86)
+        }
+        
+        placeDescription.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(610)
+            $0.leading.equalToSuperview().inset(16)
+            $0.width.equalTo(293)
+            $0.height.equalTo(78)
+        }
+        
+        letsGoButton.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(728)
+            $0.leading.equalToSuperview().inset(16)
+            $0.width.equalTo(177)
+            $0.height.equalTo(53)
+        }
     }
 }
