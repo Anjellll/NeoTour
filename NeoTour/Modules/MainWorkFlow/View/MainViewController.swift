f125f75c72307437fa9f5383da37641dbadca617
Anjella <anjellakattoobekova@gmail.com>
2024-02-15 15:54:37 +0600

    Add a discoverLabel and changed the font

--- NeoTour/Modules/MainWorkFlow/View/MainViewController.swift
+++ NeoTour/Modules/MainWorkFlow/View/MainViewController.swift
@@ -11,6 +11,18 @@ class MainViewController: UIViewController {
 
     private var viewModel: MainViewModel?
     
+    private lazy var discoverLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.text = "Discover"
+        if let font = UIFont(name: "SF-Pro-Display-Black", size: 32) {
+            label.font = font
+        } else {
+            label.font = UIFont.systemFont(ofSize: 32, weight: .bold)
+        }
+        return label
+    }()
+    
     init(viewModel: MainViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
@@ -22,9 +34,33 @@ class MainViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .cyan
-       
+        
+        view.backgroundColor = .systemGray2
     }
+    
+    override func loadView() {
+        super.loadView()
+        
+        setUpUI()
+    }
+}
 
+extension MainViewController {
+    private func setUpUI() {
+        setUpSubviews()
+        setUpConstraints()
+    }
+    
+    private func setUpSubviews() {
+        view.addSubview(discoverLabel)
+    }
+    
+    private func setUpConstraints() {
+        discoverLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(48)
+            $0.leading.equalToSuperview().inset(16)
+            $0.width.equalTo(139)
+            $0.height.equalTo(38)
+        }
+    }
 }
